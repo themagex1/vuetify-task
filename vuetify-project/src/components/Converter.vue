c97e2966cdafb3592122596d478e722166787e8b
Damian Rosiak <rosiakd6@gmail.com>
2023-05-22 23:21:51 +0200

    final

--- vuetify-project/src/components/Converter.vue
+++ vuetify-project/src/components/Converter.vue
@@ -1,35 +1,41 @@
 <template>
-  <v-container class="bg-blue-lighten-5">
-    <v-row justify="center" class="d-flex flex-column">
-      <v-col class="text-center">
-        <h1>
-          Currency converter
-        </h1>
-        <p>
-          Get current rates using our calculator
-        </p>
-      </v-col>
-      <v-col>
-        <div class="d-flex justify-center">
-          <v-sheet width="40rem">
-            <v-text-field label="Amount" v-model="amount" variant="underlined" class="bg-blue-lighten-5"></v-text-field>
-          </v-sheet>
-        </div>
-        <div class="d-flex justify-center">
-          <v-sheet width="40rem">
-            <v-select label="Choose currency" :items="computedList" v-model="firstCurrency" variant="underlined" class="bg-blue-lighten-5"></v-select>
-          </v-sheet>
-        </div>
-        <div class="d-flex justify-center">
-          <v-sheet width="40rem">
-            <v-select label="Choose currency" :items="computedList" v-model="secondCurrency" variant="underlined" class="bg-blue-lighten-5"></v-select>
-          </v-sheet>
-        </div>
+  <v-container class="bg-blue-lighten-5 py-16 h-75 w-75 rounded-shaped border-double border-xl elevation-24" fluid>
+    <v-col class="text-center">
+      <h1 class="text-h2 font-weight-bold pb-4">
+        Konwerter walut
+      </h1>
+      <p class="pb-6 text-subtitle-1">
+        Sprawdź aktualne stawki za pomocą naszego kalkulatora
+      </p>
+    </v-col>
+    <v-col>
+      <div class="d-flex justify-center">
+        <v-sheet width="40rem">
+          <v-text-field label="Kwota" v-model="amount" variant="outlined" class="bg-blue-lighten-5" type="number" min="0"></v-text-field>
+        </v-sheet>
+      </div>
+      <div class="d-flex justify-center">
+        <v-sheet width="40rem">
+          <v-select label="Z" :items="computedList" v-model="firstCurrency" variant="outlined" clearable
+            class="bg-blue-lighten-5"></v-select>
+        </v-sheet>
+      </div>
+      <div class="d-flex justify-center">
+        <v-sheet width="40rem">
+          <v-select label="Na" :items="computedList" v-model="secondCurrency" variant="outlined" clearable
+            class="bg-blue-lighten-5 pb-8"></v-select>
+        </v-sheet>
+      </div>
 
-      </v-col>
-    </v-row>
-
-
+    </v-col>
+    <v-col class="d-flex justify-center">
+      <v-sheet class="bg-blue-lighten-5 px-14 py-2 rounded-shaped" border="md">
+        <div class="font-weight-bold">
+          Wynik
+        </div>
+        {{ result }}
+      </v-sheet>
+    </v-col>
 
   </v-container>
 </template>
@@ -42,27 +48,46 @@ const currencyStore = useCurrencyStore()
 
 currencyStore.fetchCourses()
 
-const currency = ref(currencyStore.courses)
+interface courseObject {
+  currency: string,
+  mid: number
+}
 
-const amount = ref(0)
+const amount = ref(1)
 const firstCurrency = ref('')
 const secondCurrency = ref('')
-
-const currencyList = ref([] as any[])
-const computedList = computed(() => {
-  for (let i in currency.value[0]) {
-    for (let j in currency.value[0].rates) {
-     
-      currencyList.value.push(currency.value[0].rates[j].currency)
+const currencyList = ref<courseObject[]>([] as any[])
+const computedList = computed<string[]>(() => {
+  for (let i in currencyStore.courses) {
+    for (let j in currencyStore.courses[i].rates) {
+      currencyList.value.push(
+        {
+          currency: currencyStore.courses[i].rates[j].currency,
+          mid: currencyStore.courses[i].rates[j].mid
+        })
     }
   }
-  return currencyList.value
+  firstCurrency.value = currencyList.value.map(obj => obj.currency)[0]
+  secondCurrency.value = currencyList.value.map(obj => obj.currency)[1]
+  return currencyList.value.map(obj => obj.currency)
 })
 
+const result = computed<string | number>(() => {
+  if (firstCurrency.value !== '' && secondCurrency.value !== '' && amount.value >= 0) {
+    return (currencyList.value.filter(obj => obj.currency === firstCurrency.value).map(obj => obj.mid)[0]
+      /
+      currencyList.value.filter(obj => obj.currency === secondCurrency.value).map(obj => obj.mid)[0] 
+      * 
+      amount.value).toFixed(2)
+  }
+  else {
+    if(amount.value < 0) {
+      return 'Kwota musi przekraczać zero'
+    }
+    return 0
+    
+  }
 
-
-
-
-
+})
 
 </script>
--- vuetify-project/src/store/currency.ts
+++ vuetify-project/src/store/currency.ts
@@ -4,7 +4,6 @@ import axios from 'axios';
 export const useCurrencyStore = defineStore("currency", {
     state: () => ({
         courses: [] as any[]
-
     }),
     actions: {
         async fetchCourses() {
